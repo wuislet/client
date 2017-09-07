6207f3c1616d434427c4fcb2face81fb77f653d7
YLancer <15221236405@163.com>
2017-09-07 17:39:24 +0800

    公告栏循环播放

---
+++ Assets/showNotice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showNotice : MonoBehaviour {
+
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 
+  public void   show()
+    {
+        //if (this.gameObject == null)
+        this.gameObject.SetActive(true);
+    }
+    public void hide()
+    {
+        if (this.gameObject!= null)
+            this.gameObject.SetActive(false);
+    }
+}
