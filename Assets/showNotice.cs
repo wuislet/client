38296da574fcb625c518e8ee7b4753a5e6b71200
wuislet <wuislet@gmail.com>
2017-09-07 17:53:16 +0800

    test

--- Assets/showNotice.cs
+++ Assets/showNotice.cs
@@ -3,19 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class showNotice : MonoBehaviour {
-
-   
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
- 
   public void   show()
     {
         //if (this.gameObject == null)
