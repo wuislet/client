9b534559fbffe2e938bbd9e88de0476e97292e42
YLancer <15221236405@163.com>
2017-09-13 18:04:07 +0800

    XiazuiVO.cs

---
+++ Assets/Script/netManager/request/XiazuiRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+namespace AssemblyCSharp
+{
+    public class XiazuiRequest : ClientRequest
+    {
+        public XiazuiRequest(XiazuiVO xiazuiVO)
+        {
+            headCode = APIS.XIAZUI_REQUEST;
+            messageContent = JsonMapper.ToJson(xiazuiVO); ;
+        }
+    }
+}
