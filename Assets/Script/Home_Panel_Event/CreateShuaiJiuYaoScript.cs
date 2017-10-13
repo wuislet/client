d5d1f4c42f3fdebe336686e972ef87115558faad
YLancer <15221236405@163.com>
2017-10-13 11:11:27 +0800

    登录修改

--- Assets/Script/Home_Panel_Event/CreateShuaiJiuYaoScript.cs
+++ Assets/Script/Home_Panel_Event/CreateShuaiJiuYaoScript.cs
@@ -29,74 +29,74 @@ public class CreateShuaiJiuYaoScript : MonoBehaviour
         GlobalDataScript.sjyVo = SJYvo;
     }
 
-    public void onDealShuaiJiuYaoCallback(ClientResponse SJYrespone) //甩九幺的事件是在发牌之后发生的  TODO 
-    {      
-        StartGameVO sgvo = JsonMapper.ToObject<StartGameVO>(SJYrespone.message);
-        SJYpaiList = sgvo.paiArray;
+    //public void onDealShuaiJiuYaoCallback(ClientResponse SJYrespone) //甩九幺的事件是在发牌之后发生的  TODO 
+    //{      
+    //    StartGameVO sgvo = JsonMapper.ToObject<StartGameVO>(SJYrespone.message);
+    //    SJYpaiList = sgvo.paiArray;
         
-        for (int i = 0;  i < SJYpaiList[0].Count; i++)
-        {
-            int a = SJYpaiList[0][i] % 9; //扔掉幺，九牌型
-            int b = SJYpaiList[0][i] - 26;//扔掉东南西北中发白
+    //    for (int i = 0;  i < SJYpaiList[0].Count; i++)
+    //    {
+    //        int a = SJYpaiList[0][i] % 9; //扔掉幺，九牌型
+    //        int b = SJYpaiList[0][i] - 26;//扔掉东南西北中发白
             
-            if (a == 1 || b > 0 || a == 1 && b > 0)
-            {
-                dragFlag = true;
-                pai[SJYpaiList[0][i]].transform.localPosition = new Vector3 (transform.localPosition.x,-122f,transform.localPosition.z);
-                Shuaipai[SJYpaiList[0][i]].transform.position = Input.mousePosition;
+    //        if (a == 1 || b > 0 || a == 1 && b > 0)
+    //        {
+    //            dragFlag = true;
+    //            pai[SJYpaiList[0][i]].transform.localPosition = new Vector3 (transform.localPosition.x,-122f,transform.localPosition.z);
+    //            Shuaipai[SJYpaiList[0][i]].transform.position = Input.mousePosition;
 
-                if (GlobalDataScript.isDrag &&  selected == false)
-                {
-                    selected = true;
-                    oldPosition = Shuaipai[SJYpaiList[0][i]].transform.localPosition;
-                }
-                else if (Shuaipai[SJYpaiList[0][i]].transform.localPosition.y > -122f)
-                {
-                    if (bankerID == sgvo.bankerId)
-                    {
-                        GlobalDataScript.gamePlayPanel = PrefabManage.loadPerfab("Prefab/SJY_tishi_zhuang"); // 庄家甩4张、7张、10张 九幺牌          
-                        if (SJYpaiList[0].Count == 4)
-                        {
-                            Shuaipai[SJYpaiList[0][i]].onDealShuaiJiuYaoCallback += cardChange.cardChange(gameObject);
-                            Shuaipai[SJYpaiList[0][i]].onShuaiJiuYaoCallBack += cardSelect.cardChange(gameObject);
-                        }
-                        else if (SJYpaiList[0].Count == 7)
-                        {
+    //            if (GlobalDataScript.isDrag &&  selected == false)
+    //            {
+    //                selected = true;
+    //                oldPosition = Shuaipai[SJYpaiList[0][i]].transform.localPosition;
+    //            }
+    //            else if (Shuaipai[SJYpaiList[0][i]].transform.localPosition.y > -122f)
+    //            {
+    //                if (bankerID == sgvo.bankerId)
+    //                {
+    //                    GlobalDataScript.gamePlayPanel = PrefabManage.loadPerfab("Prefab/SJY_tishi_zhuang"); // 庄家甩4张、7张、10张 九幺牌          
+    //                    if (SJYpaiList[0].Count == 4)
+    //                    {
+    //                        Shuaipai[SJYpaiList[0][i]].onDealShuaiJiuYaoCallback += cardChange.cardChange(gameObject);
+    //                        Shuaipai[SJYpaiList[0][i]].onShuaiJiuYaoCallBack += cardSelect.cardChange(gameObject);
+    //                    }
+    //                    else if (SJYpaiList[0].Count == 7)
+    //                    {
                           
-                        }
-                        else if (SJYpaiList[0].Count == 10)
-                        {
+    //                    }
+    //                    else if (SJYpaiList[0].Count == 10)
+    //                    {
                             
-                        }
-                    }
-                    else
-                    {
-                        GlobalDataScript.gamePlayPanel = PrefabManage.loadPerfab("Prefab/SJY_tishi_player"); // 普通玩家甩3张、6张、9张 九幺牌
-                        if (SJYpaiList.Count == 3)
-                        {
+    //                    }
+    //                }
+    //                else
+    //                {
+    //                    GlobalDataScript.gamePlayPanel = PrefabManage.loadPerfab("Prefab/SJY_tishi_player"); // 普通玩家甩3张、6张、9张 九幺牌
+    //                    if (SJYpaiList.Count == 3)
+    //                    {
                      
-                        }
-                        else if (SJYpaiList.Count == 6)
-                        {
+    //                    }
+    //                    else if (SJYpaiList.Count == 6)
+    //                    {
                            
-                        }
-                        else if (SJYpaiList.Count == 9)
-                        {
+    //                    }
+    //                    else if (SJYpaiList.Count == 9)
+    //                    {
                            
-                        }
-                    }
-                }
-                else
-                {
-                    if (dragFlag)
-                    {
-                        Shuaipai[i].transform.localPosition = oldPosition;
-                    }
-                }
-                dragFlag = false;
-            }
-        }
-    }
+    //                    }
+    //                }
+    //            }
+    //            else
+    //            {
+    //                if (dragFlag)
+    //                {
+    //                    Shuaipai[i].transform.localPosition = oldPosition;
+    //                }
+    //            }
+    //            dragFlag = false;
+    //        }
+    //    }
+    //}
 
     public void gameReadyNotice(ClientResponse response)
     {
@@ -106,14 +106,14 @@ public class CreateShuaiJiuYaoScript : MonoBehaviour
     {
         SocketEventHandle.getInstance().StartGameNotice += gameReadyNotice;
         SocketEventHandle.getInstance().SJYCallBack += onShuaiJiuYaoCallBack;
-        SocketEventHandle.getInstance().DealSJYCallBack += onDealShuaiJiuYaoCallback;
+        //SocketEventHandle.getInstance().DealSJYCallBack += onDealShuaiJiuYaoCallback;
     }
 
     public void removeListener()
     {
         SocketEventHandle.getInstance().StartGameNotice -= gameReadyNotice;
         SocketEventHandle.getInstance().SJYCallBack -= onShuaiJiuYaoCallBack;
-        SocketEventHandle.getInstance().DealSJYCallBack -= onDealShuaiJiuYaoCallback;
+        //SocketEventHandle.getInstance().DealSJYCallBack -= onDealShuaiJiuYaoCallback;
     }
 }
 
--- Assets/Script/Login_Panle_Event/LoginSystemScript.cs
+++ Assets/Script/Login_Panle_Event/LoginSystemScript.cs
@@ -17,7 +17,7 @@ public class LoginSystemScript : MonoBehaviour {
 
 	public Text versionText;
 
-    // public InputField testloginID;   测试用的数字id登录
+    public InputField testloginID;   //测试用的数字id登录
 
 	private int tapCount = 0;//点击次数
 	public GameObject watingPanel;
@@ -150,26 +150,26 @@ public class LoginSystemScript : MonoBehaviour {
     }
 	public void doLogin(){
         StartCoroutine(ConnectTime(10, 0));
-		#if UNITY_EDITOR
+		//#if UNITY_EDITOR
         	//用于测试 不用微信登录
-        	CustomSocket.getInstance().sendMsg(new LoginRequest(null));
-		#else
-        	GlobalDataScript.getInstance ().wechatOperate.login ();
-		#endif
-        //if (testloginID.text == "null")     测试登录
-        //{
-        //    CustomSocket.getInstance().sendMsg(new LoginRequest(null));
-        //}
-        //else if(!string.IsNullOrEmpty(testloginID.text))
-        //{
-        //    int num = int.Parse(testloginID.text);
-        //    CustomSocket.getInstance().sendMsg(new LoginRequest(num));
-        //}
-        //else
-        //{
-        //    GlobalDataScript.getInstance().wechatOperate.login();
-        //}
-	}
+        	//CustomSocket.getInstance().sendMsg(new LoginRequest(null));
+//#else
+        	//GlobalDataScript.getInstance ().wechatOperate.login ();
+//#endif
+        if (testloginID.text == "null") //测试登录
+        {
+            CustomSocket.getInstance().sendMsg(new LoginRequest(null));
+        }
+        else if (!string.IsNullOrEmpty(testloginID.text))
+        {
+            int num = int.Parse(testloginID.text);
+            CustomSocket.getInstance().sendMsg(new LoginRequest(num));
+        }
+        else
+        {
+            GlobalDataScript.getInstance().wechatOperate.login();
+        }
+    }
 
 	public void LoginCallBack(ClientResponse response){
 		if (watingPanel != null) {
